556a075f63fa0239b3a175b8cc6420b18cc526f3
Yiga Gilbert <gilbert.yiga@students.mak.ac.ug>
2022-10-20 07:25:41 +0300

    updates
    updates

--- lib/screens/user_req.dart
+++ lib/screens/user_req.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ug_blood_donate/utils/firebase.dart';
 
@@ -11,6 +12,11 @@ class _UserNotificationState extends State<UserNotification> {
   final Stream<QuerySnapshot> _notificationStream = FirebaseFirestore.instance
       .collection("bloodrequests")
       .snapshots(includeMetadataChanges: true);
+
+  var fullname;
+  var photoUrl;
+  var bloodType;
+  var location;
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +36,28 @@ class _UserNotificationState extends State<UserNotification> {
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
               Map<String, dynamic> data =
                   document.data()! as Map<String, dynamic>;
+              //Map<String, dynamic> id = document.id as Map<String, dynamic>;
+              // FirebaseFirestore.instance
+              //     .collection('users')
+              //     .doc(document
+              //         .reference.id) //FirebaseAuth.instance.currentUser!.uid)
+              //     .get()
+              //     .then((value) {
+              //   //print('$id');
+              //   if (value.exists) {
+              //     print('hi diiiii');
+              //     setState(() {
+              //       fullname = value.data()!["fullname"];
+              //       location = value.data()!["location"];
+              //       photoUrl = value.data()!["photoURL"];
+              //       bloodType = value.data()!["bloodType"];
+              //     });
+              //   }
+              //   print(fullname);
+              // });
               return ListTile(
-                // leading: Text(usersRef.toString()),
-                title: Text(data['contact']),
+                // leading: Image(image: photoUrl),
+                // title: Text(fullname),
                 subtitle: Text(data['note']),
                 trailing: Text(data['location']),
               );
